0921370ab2bcb8aa4269bb93ec91d9b7557b49e0
John McDonagh <johnmcdona1234@gmail.com>
2025-04-09 19:59:26 +0100

    initial setup

---
+++ src/components/Home.js
@@ -0,0 +1,36 @@
+// Import React library
+
+import React from 'react';
+
+ 
+
+// Import external CSS file
+
+import '../App.css';
+
+ 
+
+function Home() {
+
+  return (
+
+    <div className="home">
+
+      <h1>Welcome to the Energy App</h1>
+
+      <p>This application provides information about various renewable energy sources including wind, solar, hydro, and wave energy.</p>
+
+      <p>Navigate through the app using the links in the navbar to learn more about each energy source and how they contribute to a sustainable future.</p>
+
+    </div>
+
+  );
+
+}
+
+ 
+
+// Export Home component as default
+
+export default Home;
+
