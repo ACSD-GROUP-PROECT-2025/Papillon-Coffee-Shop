9aa22a217e01f31bdafe05dc8f8f7f7f198d0e96
jkinlan <jkinlan@kravemedia.ie>
2025-04-10 18:23:39 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,11 @@ function Footer() {
             <nav>
             <h4>© EnergyInformation.net 2025</h4>
             <br></br>
+            <h4>192 Parnell Street, D01 XW18</h4>
+            <br></br>
             <Link to="/contact"><h4>Contact Us</h4></Link>
+            <br></br>
+            <Link to="https://www.instagram.com/papilloncafedublin"><h4>Instragram</h4></Link>
             </nav>
         </div>
     )
