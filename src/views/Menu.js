a5b63966b622c881c82ba34ddc4141f0f131d58c
collegemail2025ms <collegemail2025ms@gmail.com>
2025-04-10 21:54:26 +0100

    added bootstrap+manu page

--- src/views/Menu.js
+++ src/views/Menu.js
@@ -1,29 +1,63 @@
-import PageLayout from "../components/PageLayout";
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 
-const Menu = () => (
-  <PageLayout
-    title="Geothermal Energy"
-    imageSrc="https://upload.wikimedia.org/wikipedia/commons/9/9f/NesjavellirPowerPlant_edit2.jpg"
-    description="Geothermal energy is thermal energy extracted from the Earth's crust. It combines energy from the 
-        formation of the planet and from radioactive decay. Geothermal energy has been exploited as a source 
-        of heat and/or electric power for millennia.
-         <br></br>
-        Geothermal heating, using water from hot springs, for example, has been used for bathing since Paleolithic 
-        times and for space heating since Roman times. Geothermal power (generation of electricity from geothermal 
-        energy), has been used since the 20th century. Unlike wind and solar energy, geothermal plants produce power 
-        at a constant rate, without regard to weather conditions. Geothermal resources are theoretically more than 
-        adequate to supply humanity's energy needs. Most extraction occurs in areas near tectonic plate boundaries.
-         <br></br>
-        The cost of generating geothermal power decreased by 25% during the 1980s and 1990s. Technological advances 
-        continued to reduce costs and thereby expand the amount of viable resources. In 2021, the US Department of 
-        Energy estimated that power from a plant 'built today' costs about $0.05/kWh.
-         <br></br>
-        In 2019, 13,900 megawatts (MW) of geothermal power was available worldwide. An additional 28 gigawatts 
-        provided heat for district heating, space heating, spas, industrial processes, desalination, and agricultural 
-        applications as of 2010. As of 2019 the industry employed about one hundred thousand people.
-         <br></br>"
-    sourceLink="https://en.wikipedia.org/wiki/Geothermal_energy"
-  />
-);
+const Menu = () => {
+  const menuDescription = `
+    <h3>☕ Coffee</h3>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Espresso – €3.00</li>
+      <li class="list-group-item">Americano – €3.20</li>
+      <li class="list-group-item">Cappuccino – €3.80</li>
+      <li class="list-group-item">Latte – €3.80</li>
+      <li class="list-group-item">Flat White – €3.50</li>
+      <li class="list-group-item">Cortado – €3.20</li>
+      <li class="list-group-item">Chai Latte – €4.00</li>
+      <li class="list-group-item">Dirty Chai Latte – €4.50</li>
+      <li class="list-group-item">Hot Chocolate – €3.80</li>
+      <li class="list-group-item">Matcha – €4.00</li>
+    </ul>
+    <br>
+    <h3>🥤 Cold Drinks</h3>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Coca Cola / Sprite / San Pellegrino / Fanta – €2.90</li>
+      <li class="list-group-item">Mineral Water – €2.50</li>
+    </ul>
+    <br>
+    <h3>🍰 Sweets</h3>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Cake (Chocolate, Lemon, Carrot, Toffee) – €3.90</li>
+      <li class="list-group-item">Pastry (Cinnamon bun, Croissants, Caramel bites, Baklava) – €3.30</li>
+    </ul>
+    <br>
+    <h3>🥪 Food</h3>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Toastie Sandwich (Chicken/Bacon, Tuna Melt, Ham/Cheese, Triple Cheese) – €4.50</li>
+      <li class="list-group-item">Hot Dog – €4.50</li>
+    </ul>
+    <br>
+    <h3>🔥 Promotions</h3>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Coffee + Pastry – €4.95</li>
+      <li class="list-group-item">Coffee + Sandwich – €5.95</li>
+      <li class="list-group-item">Coffee + Cake – €5.95</li>
+    </ul>
+  `;
+
+  return (
+    <div className="container d-flex flex-column align-items-center justify-content-center text-center">
+      <h2>Coffee Menu</h2>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/4/45/A_small_cup_of_coffee.JPG"
+        alt="Coffee"
+        className="img-fluid mb-4"
+        style={{ maxWidth: "400px" }} 
+      />
+      <div
+        className="description"
+        dangerouslySetInnerHTML={{ __html: menuDescription }}
+      />
+    </div>
+  );
+};
 
 export default Menu;
