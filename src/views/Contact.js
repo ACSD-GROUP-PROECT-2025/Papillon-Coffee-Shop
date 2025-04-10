9cbe1b42c6782a064320a9dca8765e6cbf1845b8
jkinlan <jkinlan@kravemedia.ie>
2025-04-10 11:02:46 +0100

    CSS and Contact Us

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,11 +1,9 @@
 const Contact = () => (
     <div className="page">
-      <h2>Contact Us</h2>
-      <p>Have questions about sustainable energy or want to share your thoughts? Whether you're curious about 
-        the technologies we showcase or have ideas for future innovations, we're here to connect.
+      <h2>Get in Touch – Let’s Brew Some Conversation!</h2>
+      <p>At Papillon Café, we believe the best connections start over coffee — whether you want to share your latest coffee obsession, spill the beans, or just want to perk our ears.
         <br></br><br></br>
-        Fill out the form below, and we'll get back to you as soon as possible. Together, we can power a 
-        greener future!</p>
+        Drop us a message below, and we’ll get back to you faster than a pour-over drips. <br></br>(Okay, maybe not that fast, but we promise no one here sleeps on emails — too much espresso, you see.)</p>
       <form action="mailto:x24172901@student.ncirl.ie?subject=EnergyInformation.net%20Contact%20Form" method="post" enctype="text/plain">
         <label>Name: <input type="text" name="Name " required /></label><br></br><br></br>
         <label>Email: <input type="email" name="Email " required /></label><br></br><br></br>
