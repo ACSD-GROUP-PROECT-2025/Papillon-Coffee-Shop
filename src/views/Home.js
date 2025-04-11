e80eeda6a6fee2a667408e9f16d29e87a3fe3bf0
John McDonagh <johnmcdona1234@gmail.com>
2025-04-11 11:47:17 +0100

    moved-homepage2

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,20 +1,36 @@
-const Home = () => (
-    <div className="page">
-      <h1>Welcome to Energy Information</h1>
-      <img rel src="https://upload.wikimedia.org/wikipedia/commons/6/64/ENERNOVARE_2.png" alt="Energy on Earth" className="home" />
-      <br></br>
-      <p>Welcome to <b>EnergyInformation.net</b>, where we explore innovative technologies that harness clean, 
-        renewable energy. As the demand for energy grows, the need for sustainable and renewable sources has never 
-        been more urgent. Through innovation and cutting-edge research, we can reduce our dependence on fossil 
-        fuels and embrace cleaner alternatives that protect our planet for future generations.
-        <br></br><br></br>
-        On this platform, you’ll discover a range of energy solutions—from the proven power of solar and wind 
-        to emerging technologies like geothermal and fusion energy. Each solution holds the potential to 
-        transform how we produce and consume energy while reducing environmental impact.
-        <br></br><br></br>
-        Join us on this journey to understand these exciting advancements and how they can lead us toward a 
-        more sustainable future. Explore, learn, and be part of the change!</p>
-        </div>
+// Import React library
+
+import React from 'react';
+
+ 
+
+// Import external CSS file
+
+import '../App.css';
+
+ 
+
+function Home() {
+
+  return (
+
+    <div className="home">
+
+      <h1>Welcome to the Papillion Coffee Shop App</h1>
+
+      <p>This application provides information about the Papillion Coffee Shop</p>
+
+      <p>Navigate through the app using the links in the navbar to learn more about the Papillion Coffee Shop experince!</p>
+
+    </div>
+
   );
-  
-  export default Home;
+
+}
+
+ 
+
+// Export Home component as default
+
+export default Home;
+
